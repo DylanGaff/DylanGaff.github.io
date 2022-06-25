f3efdd9b8298263c1bb5dd9d0272344c4afb9a92
DylanGaff <102431405+DylanGaff@users.noreply.github.com>
2022-06-24 20:38:11 -0400

    fixed slideshow

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,13 +13,14 @@ showSlides(slideIndex);
 
 // This function changes the slide when the left or right arrows are clicked
 function plusSlides(n) {
-    showSlides(slideIndex+= n);
+    showSlides(slideIndex += n);
 }
 
 // This function changes the slide when the dots are clicked
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
 
 function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides"); //This takes all elements with the class name "mySLides" and stores them in the variable array "slides"
@@ -29,7 +30,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
-    for (i = 0; i <dots.length; i++) {
+    for (i = 0; i< dots.length; i++ ) {
         dots[i].className = dots[i].className.replace(" active",""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
